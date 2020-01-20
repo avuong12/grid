c1b196352f5f196ce43cab29b11bc297bfbc1076
Angela Vuong <vuong.angela@gmail.com>
2020-01-19 21:09:03 -0500

    added get, set, and fill functions to Grid

--- grid.js
+++ grid.js
@@ -1,3 +1,36 @@
-function hello() {
-    console.log('hello');
+class Grid {
+    constructor(rows, columns, defaultValue){
+        this.rows = rows;
+        this.columns = columns;
+
+        // Create a matrix with dimensions rows x columns
+        // with undefined values in each cell.
+        this.grid = [];
+        for (let r = 0; r < this.rows; ++r) {
+            let row = [];
+            for (let c = 0; c < this.columns; ++c) {
+                row.push(defaultValue);
+            }
+            this.grid.push(row);
+        }
+    }
+
+    // get function will return the value in a cell at the input coordinate
+    get(row, column) {
+        return this.grid[row][column];
+    }
+    
+    // set function will change the value of a cell.
+    set(row, column, value) {
+        this.grid[row][column] = value;
+    }
+
+    // fill function will full the entire grid with a value.
+    fill(value) {
+        for( let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.columns; j++){
+                this.set(i, j, value);
+            }
+        }
+    }
 }
