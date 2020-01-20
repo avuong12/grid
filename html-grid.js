d2b0d8522b88e96a35793fae5c913b4249e71956
Angela Vuong <vuong.angela@gmail.com>
2020-01-19 23:43:41 -0500

    finished draw function and added toggle function

--- html-grid.js
+++ html-grid.js
@@ -5,12 +5,50 @@
 // The class will draw a grid to the html.
 class HtmlGrid {
     constructor(rows, columns) {
-        this.grid = new Grid(rows, columns);
+        this.grid = new Grid(rows, columns, false);
     }
 
+    // Toggle the color of the cell at the input row and column.
+    toggleCell(row, column) {
+        // if(this.grid.get(row, column)){
+        //     this.grid.set(row, column, false);
+        // } else{
+        //     this.grid.set(row, column, true);
+        // }
+        // First update the model.
+        let currentValue = this.grid.get(row, column);
+        let newValue = !currentValue;
+        this.grid.set(row, column, newValue);
+
+        // Then update the DOM.
+        let cell = document.getElementById(`${row},${column}`);
+        // The cell is have either filled or empty. Toggling both will swap 
+        // one for the other.
+        cell.classList.toggle('filled-grid-cell');
+        cell.classList.toggle('empty-grid-cell');
+    }
+
+    // draw function will iterate through each cell in the grid.
+    // It will draw a cell for each cell coordinate. If the value
+    // of the cell is true, draw a filled cell. If the value of
+    // the cell is false, draw an empty cell. 
     draw(parentNode) {
-       let newSpan = document.createElement('span');
-       newSpan.innerText = 'hello';
-       parentNode.appendChild(newSpan);
+       for (let r = 0; r < this.grid.rows; r++){
+           for (let c = 0; c < this.grid.columns; c++){
+               let newSpan = document.createElement('span');
+               // IMPORTANT! This makes each cell acessiable with a unique id. 
+               newSpan.id = `${r},${c}`;
+               newSpan.classList.toggle('grid-cell');
+               if(this.grid.get(r, c)){
+                   newSpan.classList.toggle('filled-grid-cell');
+               } else {
+                   newSpan.classList.toggle('empty-grid-cell');
+               }
+               parentNode.appendChild(newSpan);
+           }
+
+           let breakNode = document.createElement('br');
+           parentNode.appendChild(breakNode);
+       }
     }
 }
